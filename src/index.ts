da5ffec35f28cb49ea4bee2a9f5a63fc0a89c904
NunesNathan <nunesxd7@hotmail.com>
2022-05-14 13:45:22 -0300

    :card_file_box: (database) - Connection mysql

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,6 @@ import app from './app';
 
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('é o trybesmooth');
-});
-
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
 ));
