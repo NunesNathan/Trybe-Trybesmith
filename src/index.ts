d7b1423c56338cb35af96018eb4c468bd40a0aa1
NunesNathan <nunesxd7@hotmail.com>
2022-05-14 11:46:41 -0300

    :tada: (setup) - Install dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ import app from './app';
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+  res.send('é o trybesmooth');
+});
+
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
 ));
