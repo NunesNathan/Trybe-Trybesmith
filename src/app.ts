1d1fd9e61e2b6983d6c44955d2a0e0ac40edd80b
NunesNathan <nunesxd7@hotmail.com>
2022-05-14 22:12:46 -0300

    :goal_net: (middleware) - Use error treatment

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import ErrorMiddleware from './middlewares/error/errorsMiddleware';
 import storeRouter from './routers/store';
 
 const app = express();
+const middleware = new ErrorMiddleware();
 
 app.use(express.json());
+
 app.use(storeRouter);
+
+app.use(middleware.errorTreatment);
 
 export default app;
