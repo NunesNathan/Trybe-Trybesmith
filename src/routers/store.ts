9d16e9d1820a7d696ab7816e18efc078192cdf55
NunesNathan <nunesxd7@hotmail.com>
2022-05-15 07:17:01 -0300

    :sparkles: (router) - All orders route

--- src/routers/store.ts
+++ src/routers/store.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import orders from '../controllers/ordersController';
 import products from '../controllers/productsController';
 import users from '../controllers/usersController';
 
@@ -6,6 +7,7 @@ const router = Router();
 router
   .get('/products', products.getAll)
   .post('/products', products.createProduct)
-  .post('/users', users.createUser);
+  .post('/users', users.createUser)
+  .get('/orders', orders.allOrders);
 
 export default router;
