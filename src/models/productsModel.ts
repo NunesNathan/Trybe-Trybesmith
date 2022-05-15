0fec2eb7a9fb7ade91699ebf14f3b1d6fd7e0f3a
NunesNathan <nunesxd7@hotmail.com>
2022-05-14 22:08:16 -0300

    :card_file_box: (model) - Insert product query

--- src/models/productsModel.ts
+++ src/models/productsModel.ts
@@ -1,5 +1,6 @@
+import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
-import { IProduct, IProductsModel } from '../interfaces/store.interface';
+import { IProduct, IProductsModel, IProductToCreate } from '../interfaces/store.interface';
 
 export default class ProductModel implements IProductsModel {
   constructor(private database = connection) { }
@@ -10,5 +11,14 @@ export default class ProductModel implements IProductsModel {
     );
 
     return products as IProduct[];
+  };
+
+  public create = async ({ name, amount }: IProductToCreate): Promise<IProduct> => {
+    const [{ insertId }] = await this.database.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) VALUES (?, ?)',
+      [name, amount],
+    );
+
+    return ({ id: insertId, name, amount }) as IProduct;
   };
 }
