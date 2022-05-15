77ef4201392824c2b66fd2f61e763ebf3d3064d5
NunesNathan <nunesxd7@hotmail.com>
2022-05-15 00:45:47 -0300

    :sparkles: (router) - Create user route

--- src/routers/store.ts
+++ src/routers/store.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import products from '../controllers/productsController';
+import users from '../controllers/usersController';
 
 const router = Router();
-
 router
   .get('/products', products.getAll)
-  .post('/products', products.createProduct);
+  .post('/products', products.createProduct)
+  .post('/users', users.createUser);
 
 export default router;
