7f9e20522d2be1f19a98dfa2c67d16a6488913cc
NunesNathan <nunesxd7@hotmail.com>
2022-05-14 22:11:14 -0300

    :sparkles: (feat) - Create product works

--- src/controllers/productsController.ts
+++ src/controllers/productsController.ts
@@ -1,13 +1,33 @@
 import { Request, Response } from 'express';
 import ProductsService from '../services/productsService';
 import { IProductsController } from '../interfaces/store.interface';
+import VerifyBody from '../middlewares/verifyBody';
 
-export default class ProductsController implements IProductsController {
+class ProductsController implements IProductsController {
   constructor(private service = new ProductsService()) { }
-  
+
   public getAll = async (_req: Request, res: Response): Promise<Response> => {
     const products = await this.service.getAll();
 
     return res.status(200).json(products);
   };
+
+  public create = async (req: Request, res: Response): Promise<Response> => {
+    const product = await this.service.create(req.body);
+
+    return res.status(201).json(product);
+  };
 }
+
+const product = new ProductsController();
+const verifyBody = new VerifyBody();
+
+const productsController = {
+  getAll: product.getAll,
+  createProduct: [
+    verifyBody.create,
+    product.create,
+  ],
+};
+
+export default productsController;
--- src/services/productsService.ts
+++ src/services/productsService.ts
@@ -1,8 +1,12 @@
 import ProductModel from '../models/productsModel';
-import { IProduct, IProductsService } from '../interfaces/store.interface';
+import { IProduct, IProductsService, IProductToCreate } from '../interfaces/store.interface';
 
 export default class ProductsService implements IProductsService {
   constructor(private model = new ProductModel()) { }
 
   public getAll = async (): Promise<IProduct[]> => this.model.getAll();
+
+  public create = async (newProduct: IProductToCreate): Promise<IProduct> => (
+    this.model.create(newProduct)
+  );
 }
